06a789615f589007e0894d9a2cb3bacfe00d4044
Matthew <matthewpancer@gmail.com>
2026-01-04 19:46:44 +0000

    chore: setup dynamic config for dev vs prod

---
+++ app.config.ts
@@ -0,0 +1,37 @@
+import "dotenv/config"
+import { ExpoConfig, ConfigContext } from "expo/config";
+
+function getExtension(variant: string): Partial<ExpoConfig> {
+  if (variant === "DEVELOPMENT") {
+    return {
+      ios: {
+        bundleIdentifier: "com.seek.seekapp.dev",
+      },
+      android: {
+        package: "com.seek.seekapp.dev",
+      },
+    };
+  }
+  if (variant === "PRODUCTION") {
+    return {};
+  }
+  throw new Error(`Invalid variant: ${variant}`);
+}
+
+export default ({ config }: ConfigContext): ExpoConfig => {
+  const extension = getExtension(process.env.RELEASE!);
+
+  return {
+    ...config,
+    name: "seek-app",
+    slug: "seek-app",
+    ios: {
+      ...config.ios,
+      ...extension.ios,
+    },
+    android: {
+      ...config.android,
+      ...extension.android,
+    },
+  };
+};
